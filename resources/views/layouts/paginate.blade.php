e7010d087e4c592ba09f0c16fb634ed7ed6370c3
sdk <sdk@sdks-Mac.local>
2017-12-17 07:45:05 -0500

    Test

--- resources/views/layouts/paginate.blade.php
+++ resources/views/layouts/paginate.blade.php
@@ -1,9 +1,9 @@
 @if ($paginator->lastPage() > 1)
 <ul class="pagination">
     <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
-        <a href="{{ $paginator->url(1) }}">Previous</a>
+        <a href="{{ $paginator->url($paginator->currentPage() - 1) }}">Previous</a>
     </li>
-    @for ($i = $paginator->currentPage(); $i <= ($paginator->lastPage() > 10 ? ($paginator->currentPage() + 5) : $paginator->lastPage() - 1); $i++)
+    @for ($i = $paginator->currentPage() - 5; $i <= ($paginator->lastPage() > 10 ? ($paginator->currentPage() + 5) : $paginator->lastPage() - 1); $i++)
         <li class="{{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
             <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
         </li>
